04efa946ddea24e7d57d705a0d1bb7222bc58a41
Matic <64820550+msc010312@users.noreply.github.com>
2021-02-02 17:57:26 -0900

    Add files via upload

---
+++ java_project_front/js/script.js
@@ -0,0 +1,27 @@
+window.onload = function() {
+	// header 스크롤 관련
+    window.addEventListener("scroll", function() {
+        const header = document.querySelector(".header");
+        header.classList.toggle("sticky", window.scrollY > 0);
+    });
+}
+
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        //options here
+        licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
+        autoScrolling: true,
+        scrollHorizontally: true,
+    });
+    const hBot = $('.h_bot');
+    // content의 높이값계산
+    let contHight = $('.content').outerHeight();
+    contHight = parseInt(contHight);
+    // 스크롤의 높이값계산
+    const scrollValue = $(document).scrollTop();
+    if(scrollValue == contHight) {
+        $(hBot).scroll(function () {
+            $(hBot).css("top", "0");
+        })
+    }
+});
